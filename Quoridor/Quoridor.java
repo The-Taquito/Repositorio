964433cd84e8598ce49cf724b036dd718cee89f4
The-Taquito <118077399+The-Taquito@users.noreply.github.com>
2022-11-18 23:36:56 -0500

    a

--- Quoridor/Quoridor.java
+++ Quoridor/Quoridor.java
@@ -1,6 +1,8 @@
 import java.io.*;
 
-public class Quoridor {
+import javax.lang.model.util.ElementScanner14;
+
+ public class Quoridor {
     public static void main(String[] args) throws IOException{
     Variables obj1 = new Variables();
     BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
@@ -8,8 +10,9 @@ public class Quoridor {
 
     System.out.println("¡Bienvenido a Quoridor!");
     obj1.ImprimirTablero();
-    System.out.println("Inicio del turno del primer jugador");
+    System.out.println("\n"+"Inicio del turno del primer jugador");
     obj1.jug1Confirm="True";
+    obj1.jug2Confirm="True";
 
     while (obj1.jug1Confirm.equals("True")) {
         try {
@@ -20,29 +23,104 @@ public class Quoridor {
                 obj1.decisionMovimiento= Integer.parseInt(lector.readLine());
                 if(obj1.decisionMovimiento==1){
                     obj1.MoverAdelante();
-                    System.out.println("El jugador 1 se movio hacia adelante");
+                    System.out.println("\n"+"El jugador 1 se movio hacia adelante"+"\n");
                     obj1.ImprimirTablero();
                     obj1.decisionMovimiento=0;
                     }
                 else if (obj1.decisionMovimiento==2) {
-                    
-                } else {
-                    System.out.println("Introduzca un numero valido");
+                    if(obj1.coordenadaX1==1){
+                        System.out.println("\n"+"No puedes moverte hacia abajo desde esta posicion"+"\n");
+                        continue;
+                    }
+                    obj1.MoverAbajo();
+                    System.out.println("\n"+"El jugador 1 se movio hacia abajo"+"\n");
+                    obj1.ImprimirTablero();
+                    obj1.decisionMovimiento=0;
+                }
+                else if(obj1.decisionMovimiento==3){
+                    if(obj1.coordenadaY1==17){
+                        System.out.println("\n"+"No puedes moverte hacia la derecha desde esta posicion"+"\n");
+                        continue;
+                    }
+                    obj1.MoverDerecha();
+                    System.out.println("\n"+"El jugador 1 se movio hacia la derecha"+"\n");
+                    obj1.ImprimirTablero();
+                    obj1.decisionMovimiento=0;
+                }
+                else if(obj1.decisionMovimiento==4){
+                    if(obj1.coordenadaY1==1){
+                        System.out.println("\n"+"No puedes moverte hacia la izquierda desde esta posicion"+"\n");
+                        continue;
+                    }
+                    obj1.MoverIzquierda();
+                    System.out.println("\n"+"El jugador 1 se movio hacia la izquierda"+"\n");
+                    obj1.ImprimirTablero();
+                    obj1.decisionMovimiento=0;
+                } 
+                else {
+                    System.out.println("\n"+"Introduzca un numero valido"+"\n");
                     continue;
                     }
-                }
+                }//fin movimientos jugador 1
             }
          catch (Exception e) {
             System.out.println("Introduzca un numero valido");
             continue;
-        }
+        }//fin turno jugador 1
         obj1.jug1Confirm="False";
+        
+    }//fin while jugador 1
+        if(obj1.coordenadaX1==17) {
+            System.out.println("¡Ganaste, jugador 1!");
+            System.out.println("\n"+"Movimientos realizados"+"\n"+obj1.historial); 
+            break;
+        }
+    while (obj1.jug2Confirm.equals("True")) {
+        try {
+            System.out.println("\n"+"Inicio del turno del segundo jugador");
+            System.out.println("¿Que deseas hacer?"+"\n"+"(Escribe el numero correspondiente a la opcion para seleccionarla)"+"\n"+"\n"+"1. Moverte"+"\n"+"2. Colocar una barrera"+"\n"+"3. Salir del juego/rendirte");
+            obj1.decision= Integer.parseInt(lector.readLine());
+            if (obj1.decision==1) {
+                System.out.println("A que direccion deseas moverte?"+"\n"+"\n"+"(Escribe el numero correspondiente a la opcion para seleccionarla)"+"\n"+"1. Moverte hacia adelante"+"\n"+"2. Moverte hacia abajo"+"\n"+"3. Moverte hacia la derecha"+"\n"+"4. Moverte hacia la izquierda");
+                obj1.decisionMovimiento= Integer.parseInt(lector.readLine());
+                
+                if(obj1.decisionMovimiento==1){
+                    obj1.MoverAdelante2();
+                    System.out.println("\n"+"El jugador 2 se movio hacia adelante"+"\n");
+                    obj1.ImprimirTablero();
+                    obj1.decisionMovimiento=0;
+                }
+                else if(obj1.decisionMovimiento==2){
+                    if(obj1.coordenadaX2==17){
+                        System.out.println("\n"+"No puedes moverte hacia abajo desde esta posicion"+"\n");
+                        continue;
+                    }
+                    obj1.MoverAbajo2();
+                    System.out.println("\n"+"El jugador 2 se movio hacia abajo"+"\n");
+                    obj1.ImprimirTablero();
+                    obj1.decisionMovimiento=0;
+                    
+                }
+                else if(obj1.decisionMovimiento==3){
+                    
+                }
+                  
+            }
+            obj1.jug2Confirm="False";
+        } catch (Exception e) {
+            System.out.println("Introduzca un numero valido");
+            continue;
+        }
+    }//fin while jugador 2
+    if(obj1.coordenadaX2==1) {
+        System.out.println("¡Ganaste, jugador 2!");
+        System.out.println("\n"+"Movimientos realizados"+"\n"+obj1.historial); 
+        break;
     }
-
-    continue;
-   } 
+    
+   }//fin while principal 
     
 
      
-    }
-}
+    }//fin main
+}//fin clase Quoridor
--- Quoridor/Variables.java
+++ Quoridor/Variables.java
@@ -2,14 +2,17 @@ import java.io.*;
 public class Variables {
     String jug1= "+";
     String jug1Confirm = "True";
+    String jug2Confirm = "True";
     int decision;
     int decisionMovimiento;
     int coordenadaY1 = 9;
     int coordenadaX1 = 1;
+    int coordenadaY2 = 9;
+    int coordenadaX2 = 17;
 
     String jug2= "^";
     String juego = "True";
-    String historial;
+    String historial="Inicio del juego";
     String[][] tablero ={{"-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"},
                         {"|"," ","|"," ","|"," ","|"," ","|",jug1,"|"," ","|"," ","|"," ","|"," ","|"},
                         {"-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"},
@@ -28,7 +31,7 @@ public class Variables {
                         {"|"," ","|"," ","|"," ","|"," ","|"," ","|"," ","|"," ","|"," ","|"," ","|"},
                         {"-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"},
                         {"|"," ","|"," ","|"," ","|"," ","|",jug2,"|"," ","|"," ","|"," ","|"," ","|"},
-                        {"-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"," ","-"},
+                        {"-","A","-","B","-","C","-","D","-","E","-","F","-","G","-","H","-","I","-"},
                         };
     public void ImprimirTablero(){
        for(int i=0; i<tablero.length;i++){
@@ -41,14 +44,46 @@ public class Variables {
     public void MoverAdelante(){
         tablero[coordenadaX1][coordenadaY1]=" ";
         coordenadaX1= coordenadaX1 +2;
-        tablero[coordenadaX1][coordenadaY1]="+";
+        tablero[coordenadaX1][coordenadaY1]=tablero[coordenadaX1][coordenadaY1]+jug1;
         historial=historial + "\n"+"El jugador 1 se movio hacia adelante";
 
         
 
     }
-    
-
-
-    
+    public void MoverAbajo(){
+        tablero[coordenadaX1][coordenadaY1]=" ";
+        coordenadaX1= coordenadaX1 -2;
+        tablero[coordenadaX1][coordenadaY1]=tablero[coordenadaX1][coordenadaY1]+jug1;
+        historial=historial + "\n"+"El jugador 1 se movio hacia atras";
+    }
+    public void MoverDerecha(){
+        tablero[coordenadaX1][coordenadaY1]=" ";
+        coordenadaY1= coordenadaY1 +2;
+        tablero[coordenadaX1][coordenadaY1]=tablero[coordenadaX1][coordenadaY1]+jug1;
+        historial=historial + "\n"+"El jugador 1 se movio hacia la derecha";
+    }
+    public void MoverIzquierda(){
+        tablero[coordenadaX1][coordenadaY1]=" ";
+        coordenadaY1= coordenadaY1 -2;
+        tablero[coordenadaX1][coordenadaY1]=tablero[coordenadaX1][coordenadaY1]+jug1;
+        historial=historial + "\n"+"El jugador 1 se movio hacia la izquierda";
+    }
+    public void MoverAdelante2(){
+        tablero[coordenadaX2][coordenadaY2]=" ";
+        coordenadaX2= coordenadaX2 -2;
+        tablero[coordenadaX2][coordenadaY2]=tablero[coordenadaX2][coordenadaY2]+jug2;
+        historial=historial + "\n"+"El jugador 2 se movio hacia adelante";
+    }
+    public void MoverAbajo2(){
+        tablero[coordenadaX2][coordenadaY2]=" ";
+        coordenadaX2= coordenadaX2 +2;
+        tablero[coordenadaX2][coordenadaY2]=tablero[coordenadaX2][coordenadaY2]+jug2;
+        historial=historial + "\n"+"El jugador 2 se movio hacia abajo";
+    }
 }
+    
+    
+
+
+    
+
